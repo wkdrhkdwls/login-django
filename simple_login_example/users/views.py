6fd05882cfd8c91e1eaf96ddf729f48dd255986a
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2022-11-19 11:26:51 +0900

    Login POST&GET

--- simple_login_example/users/views.py
+++ simple_login_example/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+from django.utils.datastructures import MultiValueDictKeyError
 # Create your views here.
 
 def login(request):
@@ -10,12 +11,22 @@ def login(request):
     }
     
     if(request.method == "GET"):
-        username = request.GET['username']
-        password = request.GET['password']
+        return render(request, 'users/login.html')
+    
+    if(request.method == "POST"):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         
-        if (username != user_data['username']):
+        
+        #blank 상태일 때는, 유저가 input을 입력하지않고 제출했을 때(유저가 실수했을떄)
+        if username=='':
+            return HttpResponse('유저 아이디를 입력해주세요')
+        if password=='':
+            return HttpResponse('유저 비밀번호를 입력해주세요')
+        
+        if(username != user_data['username']):
             return HttpResponse('유저 아이디가 올바르지 않습니다.')
-        if (password !=user_data['password']):
+        if(password != user_data['password']):
             return HttpResponse('유저 비밀번호가 올바르지 않습니다.')
         
         return HttpResponse('로그인 성공!')
