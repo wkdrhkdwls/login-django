24bf0771efffe1bd5b85e0581ad9b05b886fdcc1
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2022-12-03 12:12:39 +0900

    week9

--- simple_login_example/users/urls.py
+++ simple_login_example/users/urls.py
@@ -5,5 +5,8 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', views.login, name="login"),
-    path('login/<int:id>/', views.login_detail, name="login_detail")
+    path('login/<int:id>/', views.login_detail, name="login_detail"),
+    path('login/index', views.index, name='index'),
+    path('logout/',views.logout, name='logout'),
+    path('signup/',views.signup, name="signup"),
 ]
--- simple_login_example/users/views.py
+++ simple_login_example/users/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from users.models import User
 
 # Create your views here.
 
 
 def login(request):
-    user_data = {
-        'username': 'python',
-        'password': 'django'
-    }
-
     context = {
         'method': request.method,
         'is_valid': True
@@ -22,29 +18,59 @@ def login(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        # blank 상태일 때는, 유저가 input을 입력하지 않고 제출했을 때 (유저의 실수)
-        if username == '':
-            context['is_valid'] = False
-        if password == '':
-            context['is_valid'] = False
+        try:
+            user = User.objects.get(username=username, password=password)
 
-        if (username != user_data['username']):
-            context['is_valid'] = False
-        if (password != user_data['password']):
-            context['is_valid'] = False
-        
-        if context['is_valid']:
             response = redirect('pages:index')
             response.set_cookie('is_login', True)
-            response.set_cookie('username', user_data['username'])
-            response.set_cookie('password', user_data['password'])
+            response.set_cookie('username', user.username)
+            response.set_cookie('password', user.password)
 
             return response
+        except User.DoesNotExist:
+            context['is_valid'] = False
         return render(request, 'users/login.html', context)
 
 
+def signup(request):
+    context={
+        'blank':False,
+        'exists':False,
+    }
+    template_name="users/signup.html"
+    
+    if request.method == "GET":
+        return render(request, template_name,context)
+    
+    if request.method == "POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        #추가할 코드
+        user = User.objects.create(username=username,password=password)
+        
+        if username == "" or password=="":
+            context['blank']= True
+            return render(request,template_name,context)
+        
+        exist = User.objects.filter(username=username).exists()
+        if exist:
+            context['exists'] = exist
+            return render(request,template_name,context)
+        
+        
+        #====
+        return redirect("pages:index")
+    
 def login_detail(request, id):
     return HttpResponse('user id 는' + str(id) + '입니다.')
 
 def index(request,):
     return render(request, 'index.html')
+
+def logout(request):
+    response = redirect('pages:index')
+    response.delete_cookie('is_login')
+    response.delete_cookie('username')
+    response.delete_cookie('password')
+    
+    return response
